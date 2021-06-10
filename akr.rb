4ee954b4fbda88b5eb2dd170301df8ea6b433f88
agrinman <agrinman@users.noreply.github.com>
2021-06-10 04:28:20 +0000

    [create-pull-request] automated change

--- akr.rb
+++ akr.rb
@@ -5,8 +5,8 @@ class Akr < Formula
 
     bottle do
       root_url "https://github.com/akamai/homebrew-mfa/releases/download/1.0"
-      sha256 catalina:  "9647e1f1be7f80702d8df4c2f16f4970c363cc3dd23fc1e89899feeb62c5245e"
-      sha256 big_sur: "9647e1f1be7f80702d8df4c2f16f4970c363cc3dd23fc1e89899feeb62c5245e"
+      sha256 catalina:  "45081289f0bd25fa6fe7e2bbb2bd28b91e6737cba73523e4ede3dc30fe64c639"
+      sha256 big_sur: "45081289f0bd25fa6fe7e2bbb2bd28b91e6737cba73523e4ede3dc30fe64c639"
     end
 
     test do
