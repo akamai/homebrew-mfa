cfb592c50ce6a4a3cefbbc3c39a205c2c9bf3dc5
nikhilty <nikhilty@users.noreply.github.com>
2023-04-12 18:18:46 +0000

    [create-pull-request] automated change

--- akr.rb
+++ akr.rb
@@ -1,13 +1,13 @@
 class Akr < Formula
     desc "Developer CLI for SSH FIDO2 via Akamai MFA (kr/Krypton 2.0)"
     homepage "https://github.com/akamai/homebrew-mfa"
-    url "https://github.com/akamai/akr/archive/1.1.1.zip"
-    sha256 "13a33c6613efb38bc0d724cb4fce9c6da63dc9d8ddbb0f6168c621e108c3867b"
+    url "https://github.com/akamai/akr/archive/1.1.2.zip"
+    sha256 "f61c3f9321e2158533c579ed2ddc0cb86c9feab6d229738d8e572ebc19d703f3"
   
     bottle do
-      root_url "https://github.com/akamai/akr/releases/download/1.1.1"
-      sha256 catalina:  "dde1300b25f300eb6732a78312b7fc863add25a835b71e8d9a92de7b9ee9632a"
-      sha256 big_sur: "dde1300b25f300eb6732a78312b7fc863add25a835b71e8d9a92de7b9ee9632a"
+      root_url "https://github.com/akamai/akr/releases/download/1.1.2"
+      sha256 catalina:  "491b43ff0ea79be553ba2e9e9ebce59395ad0c81d5a8c66ca71aebc451738f0a"
+      sha256 big_sur: "491b43ff0ea79be553ba2e9e9ebce59395ad0c81d5a8c66ca71aebc451738f0a"
     end
 
     depends_on "rust" => :build
